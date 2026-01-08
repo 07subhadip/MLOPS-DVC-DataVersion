b1ccfaa4feb65bb12202a0210da108b787528537
Subhadip Hensh <subhadiphensh31102000@gmail.com>
2026-01-08 13:12:12 +0530

    Third version of data saved

--- mycode.py
+++ mycode.py
@@ -16,6 +16,13 @@ new_row_loc = {
 }
 df.loc[len(df.index)] = new_row_loc
 
+new_row_loc1 = {
+    "Name": "GF2",
+    "Age": 25,
+    "City": "Delhi"
+}
+df.loc[len(df.index)] = new_row_loc1
+
 data_dir = "data"
 os.makedirs(data_dir, exist_ok = True)
 
